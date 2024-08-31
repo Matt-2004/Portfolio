a7204ee22e7b8f4a2fda0c781314a204418cf0d8
Mas <wai450013956@gmail.com>
2024-08-31 18:50:12 +0630

    Remove: export in roboto_mono provider

--- social_media/src/app/layout.tsx
+++ social_media/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-export const roboto_Mono = Roboto_Mono({
+const roboto_Mono = Roboto_Mono({
   style: ["normal", "italic"],
   subsets: ["latin"],
 });
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={roboto_Mono.className}>{children}</body>
     </html>
   );
