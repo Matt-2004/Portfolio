df06be53e0048a688da6520cd1f9e619b604645a
Cap <wai450013956@gmail.com>
2025-07-19 22:22:24 +0700

    V1

--- social_media/src/app/page.tsx
+++ social_media/src/app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
-import Hero from "components/Hero";
-import Projects from "components/Projects";
+import Hero from "../components/Hero";
+import Projects from "../components/Projects";
 
 export default function Home() {
   return (
     <main className=''>
       <div className='w-screen min-h-full lg:h-screen lg:flex sm:relative overflow-y-auto '>
         <Hero />
-
         <Projects />
       </div>
     </main>
